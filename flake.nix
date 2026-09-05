80c80b7c49ca7b2697e0cbf891b41d6a1d989f79
tfqhkl <taufiqhaikal1808@gmail.com>
2026-09-06 00:24:55 +0800

    v1

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "emberify on nix-shell flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        pipxNoCheck = pkgs.pipx.overridePythonAttrs (old: {
+          doCheck = false;
+        });
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "emberify-pipx";
+
+          packages = with pkgs; [
+            python313
+            pipxNoCheck
+            git
+            opencl-headers
+            ocl-icd
+            stdenv.cc.cc.lib
+            zlib
+          ];
+
+          shellHook = ''
+            export PIPX_HOME="$PWD/.pipx"
+            export PIPX_BIN_DIR="$PWD/.pipx/bin"
+            export PATH="$PIPX_BIN_DIR:$PATH"
+
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc.lib
+              pkgs.zlib
+              pkgs.ocl-icd
+            ]}:$LD_LIBRARY_PATH"
+
+            if [ ! -x "$PIPX_BIN_DIR/emberify" ]; then
+              echo "Installing emberify with pipx..."
+              pipx install "git+https://github.com/ember-theme/emberify.git"
+            fi
+            echo "Shell Active!"
+          '';
+        };
+      });
+}
